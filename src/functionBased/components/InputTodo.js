6226fe6dbd7655c89eb8caf06ab34cb6e3094f0b
AbrahaKahsay <abrahak81@gmail.com>
2022-08-10 00:33:55 +0300

    Add InputTodo to function component

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,35 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 /* eslint-disable */
-class InputTodo extends Component {
-    state = {
-      title: '',
-    };
+const InputTodo = (props) => {
+  const [inputText, setInputText] = useState({
+    title: '',
+  });
 
- onChange = (e) => {
-   this.setState({
-     [e.target.name]: e.target.value,
-   });
- };
+  const onChange = (e) => {
+    setInputText({
+      ...inputText,
+      [e.target.name]: e.target.value,
+    });
+  };
 
- handleSubmit = (e) => {
-   e.preventDefault();
-   if (this.state.title.trim()) {
-     this.props.addTodoProps(this.state.title);
-     this.setState({
-       title: '',
-     });
-   } else {
-     alert('Please write item');
-   }
- };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { title } = inputText;
+    const { addTodoProps } = props;
+    if (title.trim()) {
+      addTodoProps(title);
+      setInputText({
+        title: '',
+      });
+    } else {
+      alert('Please write item');
+    }
+  };
 
- render() {
-   return (
-     <form onSubmit={this.handleSubmit} className="form-container">
-       <input type="text" className="input-text" placeholder="Add Todo..." value={this.state.title} name="title" onChange={this.onChange} />
-       <button className="input-submit">Submit</button>
-     </form>
-   );
- }
-}
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add todo..."
+        value={inputText.title}
+        name="title"
+        onChange={onChange}
+      />
+      <button type="button" className="input-submit">Submit</button>
+    </form>
+  );
+};
+
 export default InputTodo;
