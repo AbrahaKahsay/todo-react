24f7f6cc470ef1d90f8e04e0e89d89b659814141
AbrahaKahsay <abrahak81@gmail.com>
2022-08-10 00:36:46 +0300

    Add TodosList to function component

--- src/functionBased/components/TodosList.js
+++ src/functionBased/components/TodosList.js
@@ -1,21 +1,18 @@
+/* eslint-disable */
 import React from 'react';
 import TodoItem from './TodoItem';
-/* eslint-disable */
-class TodosList extends React.Component {
-  render() {
-    return (
-      <ul>
-        {this.props.todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            handleChangeProps={this.props.handleChangeProps}
-            deleteTodoProps={this.props.deleteTodoProps}
-            setUpdate={this.props.setUpdate}
-          />
-        ))}
-      </ul>
-    );
-  }
-}
+/* eslint-disable react/prop-types */
+const TodosList = (props) => (
+  <ul>
+    {props.todos.map((todo) => (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        handleChangeProps={props.handleChangeProps}
+        deleteTodoProps={props.deleteTodoProps}
+        setUpdate={props.setUpdate}
+      />
+    ))}
+  </ul>
+);
 export default TodosList;
